91966359d7d94e7da2df64ee820620c0ab0302f3
Bryn Beaudry <babeaudry@gmail.com>
2017-10-17 01:11:24 -0700

    Jetpack distance between multiple point gives the distance but no paths are being laid down, and driving or walking distance is not working. Garbage is displayed when only one annotations is detected.

--- AppleMapsDistanceRoute/ViewController.swift
+++ AppleMapsDistanceRoute/ViewController.swift
@@ -14,23 +14,45 @@ class ViewController: UIViewController, MKMapViewDelegate,  CLLocationManagerDel
     var totalDistance : Double = 0
     var locationManager:CLLocationManager!
     var userLocation:CLLocation!
+    var points: [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
+    var polyline : MKPolyline!
+    enum Trans {
+        case WALKING
+        case JETPACKING
+        case DRIVING
+    }
+    enum PopUpState {
+        case OPEN
+        case CLOSED
+    }
     
     var transportType : MKDirectionsTransportType!
+    var selectTransport : Trans = Trans.JETPACKING
+    var popUpState : PopUpState = PopUpState.CLOSED
 
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var bottomBtnBar: UIToolbar!
     
     @IBAction func driveBtn(_ sender: UIBarButtonItem) {
         transportType = MKDirectionsTransportType.automobile
+        selectTransport = Trans.DRIVING
         print("Drive!")
+        mapView.removeAnnotations(mapView.annotations)
+        points.removeAll()
     }
     @IBAction func walkBtn(_ sender: UIBarButtonItem) {
-        transportType = MKDirectionsTransportType.automobile
+        transportType = MKDirectionsTransportType.walking
+        selectTransport = Trans.WALKING
         print("Walk!")
+        mapView.removeAnnotations(mapView.annotations)
+        points.removeAll()
     }
     @IBAction func jetpackBtn(_ sender: UIBarButtonItem) {
         transportType = nil
+        selectTransport = Trans.JETPACKING
         print("Jetpack!")
+        mapView.removeAnnotations(mapView.annotations)
+        points.removeAll()
     }
     
     
@@ -46,6 +68,7 @@ class ViewController: UIViewController, MKMapViewDelegate,  CLLocationManagerDel
         //Where is the UIElem we're listening for taps? This class.
         //Where is the function to handle the action defined? In the class of the delegate/order taker
         mapLPRecognizer.delegate = self //The view is responsible to provide the function. It can modify the necessary data to carry it out
+        mapLPRecognizer.minimumPressDuration = CFTimeInterval(1.5)
         mapView.addGestureRecognizer(mapLPRecognizer)
         //I think all UI Controllers have add/remove gesture regonizers.
         
@@ -54,13 +77,19 @@ class ViewController: UIViewController, MKMapViewDelegate,  CLLocationManagerDel
         
     }
     
-    @objc func handleMapPress(gestureReconizer: UILongPressGestureRecognizer) {
+    @objc func handleMapPress(gestureRecognizer: UILongPressGestureRecognizer) {
         //converts CG Point to LocCoord2d
         print("In handlemap press")
-        let location = gestureReconizer.location(in: mapView)
-        let coordinate : CLLocationCoordinate2D = mapView.convert(location,toCoordinateFrom: mapView)
-        dropPinAtCoordinate(c: coordinate)
+        if popUpState == PopUpState.CLOSED {
+            popUpState = PopUpState.OPEN
+            self.becomeFirstResponder()
+            let location = gestureRecognizer.location(in: mapView)
+            let coordinate : CLLocationCoordinate2D = mapView.convert(location,toCoordinateFrom: mapView)
+            dropPinAtCoordinate(c: coordinate)
+            checkNumberOfAnnotations()
+        }
     }
+    
     
     func centerMapOnLocation() {
         print(userLocation)
@@ -82,19 +111,43 @@ class ViewController: UIViewController, MKMapViewDelegate,  CLLocationManagerDel
         myAnnotation.coordinate = CLLocationCoordinate2DMake(c.latitude, c.longitude);
         myAnnotation.title = "Pin at \(c.latitude), \(c.longitude)"
         mapView.addAnnotation(myAnnotation)
-        checkNumberOfAnnotations()
     }
     
-    func popUp() {
+    func dismissPopUp(_ : UIAlertAction) -> Void{
+        print("in dismiss Popup")
+        popUpState = PopUpState.CLOSED
+    }
+    
+    func popUp(message: String) {
+        print("In popup")
+        var alertText : String = "\n\n\n\n\n\n\n\n\n\n\n\n"
+        if(!message.isEmpty){
+            alertText += message
+        }
+        
+        let alertMessage = UIAlertController(title: "My Title", message: alertText, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: self.dismissPopUp)
+        alertMessage.addAction(action)
+        self.present(alertMessage, animated: true, completion: nil)
+        let xPosition = alertMessage.view.frame.origin.x + 80
+        let rectImg = #imageLiteral(resourceName: "routesymbol")
+        let rect : CGRect = CGRect(x: xPosition, y: 100, width: 100, height: 100)
+        //rectImg.draw(in: rect)
+        let imageView = UIImageView(frame: rect)
+        imageView.image = rectImg
+        alertMessage.view.addSubview(imageView)
+    }
+    
+    func garGarPopUp() {
         print("In popup")
         let alertMessage = UIAlertController(title: "My Title", message: "\n\n\n\n\n\n\n\n\n\n\n\n", preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: self.dismissPopUp)
         alertMessage .addAction(action)
         self.present(alertMessage, animated: true, completion: nil)
         let xPosition = alertMessage.view.frame.origin.x + 80
         let garbageRes = #imageLiteral(resourceName: "garbage")
         let rect : CGRect = CGRect(x: xPosition, y: 100, width: 100, height: 100)
-        garbageRes.draw(in: rect)
+        //garbageRes.draw(in: rect)
         let imageView = UIImageView(frame: rect)
         imageView.image = garbageRes
         alertMessage.view.addSubview(imageView)
@@ -103,34 +156,47 @@ class ViewController: UIViewController, MKMapViewDelegate,  CLLocationManagerDel
     //TODO: Finish this implementation
     func checkNumberOfAnnotations(){
         if mapView.annotations.count > 1 {
-            //calculateTotalDistance(annotations : [MKAnnotation]) ->
+            calculateTotalDistance(annotations : mapView.annotations)
             //show popup
-            popUp()
+            var mode : String
+            switch selectTransport {
+                case .DRIVING:
+                        mode = "driving"
+                case .JETPACKING:
+                        mode = "jetpacking"
+                case .WALKING:
+                        mode = "walking"
+            }
+            let distString = NSString(format: "%.2f", totalDistance)
+            popUp(message: "Total \(mode) Distance : \(distString)")
         }else{
             //show a different popup
+            garGarPopUp()
         }
     }
     
     
     
     func calculateTotalDistance(annotations : [MKAnnotation]) {
-        if(transportType != nil){
-            for index in 0...annotations.count-1 {
-                if(index != annotations.count){
+        totalDistance = 0
+        points.removeAll()
+        if(selectTransport != Trans.JETPACKING){
+            for index in 0...annotations.count-2 {
                     let locA = MKMapItem.init(placemark: MKPlacemark.init(coordinate: annotations[index].coordinate))
                     let locB = MKMapItem.init(placemark: MKPlacemark.init(coordinate: annotations[index + 1].coordinate))
                     getDirections(start: locA, end: locB)
-                }
             }
         }else{
-            for index in 0...annotations.count-1 {
-                if(index != annotations.count){
+            for index in 0...annotations.count-2 {
+                    points.append(annotations[index].coordinate)
                     let locA = CLLocation(latitude: annotations[index].coordinate.latitude, longitude: annotations[index].coordinate.longitude)
                     let locB = CLLocation(latitude: annotations[index+1].coordinate.latitude, longitude: annotations[index+1].coordinate.longitude)
                     totalDistance += Double(locA.distance(from: locB))
-                }
             }
+            polyline = MKPolyline(coordinates: points, count: points.count)
+            mapView.add(polyline, level: MKOverlayLevel.aboveRoads)
         }
+        
          print("Total Distance : \(totalDistance)")
         //If sleected thing is driving
     }
